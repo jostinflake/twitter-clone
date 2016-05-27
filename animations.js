c6a083aa677ec6a0c7298bba388ca78cc43a2f68
Jostin Flake <jostinflake21@gmail.com>
2016-05-26 20:44:32 -0600

    step 3

--- animations.js
+++ animations.js
@@ -3,4 +3,14 @@ $(document).ready(function(){
 		$('#tweet-controls').show();
 		$('.tweet-compose').animate({height: '5em'}, 'click');
 	});
+	$('.tweet-compose').keyup(function() {
+  		var max = 140;
+  		var len = $(this).val().length;
+  		var char = max - len;
+  		$('#char-count').html(char);
+		if (len >= (max - 10))  {
+	    	$('#char-count').css('color', 'red');
+	  	} 
+ 	 });
+		
 })
